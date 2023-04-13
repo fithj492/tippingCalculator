7a9cc120bf132d635210f99cf18dc2d7dfd9ff4b
Fithian_Justin <fithj492@hsd.k12.or.us>
2023-04-13 13:32:23 -0700

    first steps added button.

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -5,6 +5,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+
 
 import java.io.IOException;
 
@@ -17,9 +20,25 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        //scene = new Scene(loadFXML("primary"), 640, 480);
+        
+        StackPane layout = new StackPane();
+        Scene scene = new Scene(layout, 640, 480);
+        Button plusbutton = new Button("+");
+        Button minusbutton = new Button("i tried");
+        minusbutton.setPrefSize(500, 20);
         stage.setScene(scene);
         stage.show();
+        stage.setTitle("CodersLegacy");
+        layout.getChildren().addAll(plusbutton);
+        layout.getChildren().addAll(minusbutton);
+
+
+         
+
+         
+
+         
     }
 
     static void setRoot(String fxml) throws IOException {
@@ -33,6 +52,12 @@ public class App extends Application {
 
     public static void main(String[] args) {
         launch();
+
+
+
+
     }
+
+    
 
 }

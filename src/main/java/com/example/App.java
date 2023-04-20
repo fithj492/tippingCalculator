e099b582ee37c0bd34959ac9751753a78ae23c41
Fithian_Justin <fithj492@hsd.k12.or.us>
2023-04-20 13:31:04 -0700

    added gridpane

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,8 +2,15 @@ package com.example;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets; 
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
+
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane; 
+import javafx.scene.text.Text; 
+import javafx.scene.control.TextField; 
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -23,23 +30,73 @@ public class App extends Application {
         //scene = new Scene(loadFXML("primary"), 640, 480);
         
         StackPane layout = new StackPane();
-        Scene scene = new Scene(layout, 640, 480);
         Button plusbutton = new Button("+");
-        Button minusbutton = new Button("i tried");
-        minusbutton.setPrefSize(500, 20);
+        Button minusbutton = new Button("-");
+        Label label1 = new Label("hello");  
+        minusbutton.setPrefSize(20, 20);
         stage.setScene(scene);
         stage.show();
         stage.setTitle("CodersLegacy");
         layout.getChildren().addAll(plusbutton);
         layout.getChildren().addAll(minusbutton);
+        layout.getChildren().addAll(label1);
+        Text text1 = new Text("");
+        Text text2 = new Text("");
+        Text text3 = new Text("");
+        TextField textField1 = new TextField();       
+        TextField textField2 = new TextField();   
+        TextField textField3 = new TextField();  
+    
 
+        //Creating a Grid Pane 
+      GridPane gridPane = new GridPane();    
 
+      //Setting size for the pane 
+      gridPane.setMinSize(400, 200); 
+
+      //Setting the padding  
+      gridPane.setPadding(new Insets(10, 10, 10, 10)); 
+
+      //Setting the vertical and horizontal gaps between the columns 
+      gridPane.setVgap(10); 
+      gridPane.setHgap(10);       
+
+      //Setting the Grid alignment 
+      gridPane.setAlignment(Pos.CENTER); 
+
+      //Arranging all the nodes in the grid 
+      gridPane.add(text1, 0, 0); 
+      gridPane.add(textField1, 1, 0); 
+      gridPane.add(text2, 0, 1);       
+      gridPane.add(textField2, 1, 1); 
+      gridPane.add(text3, 1, 2);       
+      gridPane.add(textField3, 1, 2); 
+      gridPane.add(minusbutton, 0, 2); 
+      gridPane.add(plusbutton, 2, 2); 
+      gridPane.add(label1, 3, 0); 
+
+      //Styling nodes  
+      plusbutton.setStyle("-fx-background-color: darkslateblue; -fx-text-fill: white;"); 
+      minusbutton.setStyle("-fx-background-color: darkslateblue; -fx-text-fill: white;"); 
+
+      text1.setStyle("-fx-font: normal bold 20px 'sans-serif' "); 
+      text2.setStyle("-fx-font: normal bold 20px 'sans-serif' ");  
+      text3.setStyle("-fx-font: normal bold 20px 'sans-serif' ");  
+      label1.setStyle("-fx-font: normal bold 20px 'sans-serif' ");  
+      gridPane.setStyle("-fx-background-color: #999999;"); 
+
+      //Creating a scene object 
+      Scene scene = new Scene(gridPane); 
+
+      stage.setTitle("CSS Example");
          
-
+      stage.setScene(scene);
          
-
+      stage.show();
          
     }
+
+
 
     static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));

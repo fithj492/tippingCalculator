c97bcd720e0938ac0873f0240db7edc477b568b6
Fithian_Justin <fithj492@hsd.k12.or.us>
2023-04-21 12:00:14 -0700

    better looks new lables.

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -27,25 +27,28 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        //scene = new Scene(loadFXML("primary"), 640, 480);
         
         StackPane layout = new StackPane();
         Button plusbutton = new Button("+");
         Button minusbutton = new Button("-");
-        Label label1 = new Label("hello");  
+        Button plusbutton2 = new Button("+");
+        Button minusbutton2 = new Button("-");
+        Button plusbutton3 = new Button("+");
+        Button minusbutton3 = new Button("-");
+        Label label4 = new Label(" 1.5$ per person");  
         minusbutton.setPrefSize(20, 20);
         stage.setScene(scene);
         stage.show();
         stage.setTitle("CodersLegacy");
         layout.getChildren().addAll(plusbutton);
         layout.getChildren().addAll(minusbutton);
-        layout.getChildren().addAll(label1);
-        Text text1 = new Text("");
-        Text text2 = new Text("");
-        Text text3 = new Text("");
-        TextField textField1 = new TextField();       
-        TextField textField2 = new TextField();   
-        TextField textField3 = new TextField();  
+        layout.getChildren().addAll(label4);
+        Text text1 = new Text("cost");
+        Text text2 = new Text("percentage");
+        Text text3 = new Text("people");
+        TextField textField1 = new TextField("     15$");       
+        TextField textField2 = new TextField("     10%");   
+        TextField textField3 = new TextField("     1");  
     
 
         //Creating a Grid Pane 
@@ -67,22 +70,31 @@ public class App extends Application {
       //Arranging all the nodes in the grid 
       gridPane.add(text1, 0, 0); 
       gridPane.add(textField1, 1, 0); 
+      gridPane.add(minusbutton, 1, 0); 
+      gridPane.add(plusbutton, 3, 0); 
+      gridPane.add(label4, 4, 0); 
       gridPane.add(text2, 0, 1);       
       gridPane.add(textField2, 1, 1); 
-      gridPane.add(text3, 1, 2);       
+      gridPane.add(minusbutton2, 1, 1); 
+      gridPane.add(plusbutton2, 3, 1); 
+      gridPane.add(text3, 0, 2);       
       gridPane.add(textField3, 1, 2); 
-      gridPane.add(minusbutton, 0, 2); 
-      gridPane.add(plusbutton, 2, 2); 
-      gridPane.add(label1, 3, 0); 
+      gridPane.add(minusbutton3, 1, 2); 
+      gridPane.add(plusbutton3, 3, 2); 
+
 
       //Styling nodes  
       plusbutton.setStyle("-fx-background-color: darkslateblue; -fx-text-fill: white;"); 
       minusbutton.setStyle("-fx-background-color: darkslateblue; -fx-text-fill: white;"); 
+      plusbutton2.setStyle("-fx-background-color: darkslateblue; -fx-text-fill: white;"); 
+      minusbutton2.setStyle("-fx-background-color: darkslateblue; -fx-text-fill: white;"); 
+      plusbutton3.setStyle("-fx-background-color: darkslateblue; -fx-text-fill: white;"); 
+      minusbutton3.setStyle("-fx-background-color: darkslateblue; -fx-text-fill: white;"); 
 
-      text1.setStyle("-fx-font: normal bold 20px 'sans-serif' "); 
-      text2.setStyle("-fx-font: normal bold 20px 'sans-serif' ");  
-      text3.setStyle("-fx-font: normal bold 20px 'sans-serif' ");  
-      label1.setStyle("-fx-font: normal bold 20px 'sans-serif' ");  
+      text1.setStyle("-fx-font: normal bold 10px 'sans-serif' "); 
+      text2.setStyle("-fx-font: normal bold 10px 'sans-serif' ");  
+      text3.setStyle("-fx-font: normal bold 10px 'sans-serif' ");  
+      label4.setStyle("-fx-font: normal bold 20px 'sans-serif' ");  
       gridPane.setStyle("-fx-background-color: #999999;"); 
 
       //Creating a scene object 
